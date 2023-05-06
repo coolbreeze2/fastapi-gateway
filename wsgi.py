1e6e88b8d8eab1249cbc87ec3edf505a5177ad5e
coodyz <1848600800@qq.com>
2023-05-06 23:38:21 +0800

    feat: add k8s deployment.yaml

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,11 @@
+import os
 import uvicorn
 
 from fastapi_gateway.server.app import app
 from fastapi_gateway.settings import LOGGING_CONFIG
 
 if __name__ == '__main__':
-    uvicorn.run(app, port=80, host="0.0.0.0", log_level="info", log_config=LOGGING_CONFIG)
+    port = int(os.getenv("PORT", 80))
+    host = os.getenv("HOST", "0.0.0.0")
+    log_level = os.getenv("LOG_LEVEL", "info")
+    uvicorn.run(app, port=port, host=host, log_level=log_level, log_config=LOGGING_CONFIG)
